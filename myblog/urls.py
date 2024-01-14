c611f14c744e737d628c0578fe82d2e07b8d8703
Fidel <fidelmasitsa03@gmail.com>
2024-01-14 09:30:15 +0300

    allot

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,4 +1,6 @@
 # blog/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -14,12 +16,11 @@ urlpatterns = [
     path('blog/<int:pk>/edit/', views.blog_edit, name='blog_edit'),
     path('blog/<int:pk>/delete/', views.blog_delete, name='blog_delete'),
     path('dashboard/', views.blog_dashboard, name='blog_dashboard'),
-
-
-
 ]
 
-
+# Add the following lines to serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
