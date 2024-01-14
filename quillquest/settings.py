92393452f3b993557cbac5d844e03be271a7a4ef
Fidel <fidelmasitsa03@gmail.com>
2024-01-14 09:23:53 +0300

    mannh

--- quillquest/settings.py
+++ quillquest/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'quillquest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'myblog', 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
