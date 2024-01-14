1884e7528ba468520ea5085ef80e4ccdada14c76
Fidel <fidelmasitsa03@gmail.com>
2024-01-14 10:12:08 +0300

    last last

--- quillquest/settings.py
+++ quillquest/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
 ]
 
 
